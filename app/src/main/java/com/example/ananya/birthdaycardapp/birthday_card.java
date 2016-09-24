d181e029282233cbc40a7377e498521bac71489e
ananya <anu9597@gmail.com>
2016-09-24 17:20:17 +0530

    initial commit

---
+++ app/src/main/java/com/example/ananya/birthdaycardapp/birthday_card.java
@@ -0,0 +1,16 @@
+package com.example.ananya.birthdaycardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class birthday_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday_card);
+
+    }
+}
+
